f80ce7a995e2aba74136110d768e473d181b9584
Bogdan Sova <bb_sova@mail.ru>
2022-11-16 18:35:03 +0300

    add calc.py

---
+++ brain_games/games/calc.py
@@ -0,0 +1,20 @@
+import random
+import operator
+
+
+DESCRIPTION = 'What is the result of the expression?'
+
+
+def get_question_and_answer():
+    number_one = random.randint(1, 100)
+    number_two = random.randint(1, 100)
+    math_symbols = ['+', '-', '*']
+    math_symbol = random.choice(math_symbols)
+    question = (f'{number_one} {math_symbol} {number_two}')
+    if math_symbol == '+':
+        answer = operator.add(number_one, number_two)
+    elif math_symbol == '-':
+        answer = operator.sub(number_one, number_two)
+    elif math_symbol == '*':
+        answer = operator.mul(number_one, number_two)
+    return question, str(answer)
